c99dd3f227bd673ff24003a6918f112f15b4b615
Agu-221 <agu.ferrer-rojas@impact.com>
2026-06-01 09:45:21 +0200

    copilot stuff

--- lua/agu221/lazy/copilot_chat.lua
+++ lua/agu221/lazy/copilot_chat.lua
@@ -1,17 +1,21 @@
 return {
-	"CopilotC-Nvim/CopilotChat.nvim",
-	dependencies = {
-		{ "github/copilot.vim" },
-		{ "nvim-lua/plenary.nvim", branch = "master" },
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		dependencies = {
+			{ "nvim-lua/plenary.nvim", branch = "master" },
+		},
+		build = "make tiktoken",
+		keys = {
+			{
+				"<leader>ccq",
+				function()
+					local input = vim.fn.input("Quick Chat: ")
+					if input ~= "" then
+						require("CopilotChat").ask(input, { selection = require("CopilotChat.select").buffer })
+					end
+				end,
+				desc = "CopilotChat - Quick chat"
+			},
+		},
 	},
-	build = "make tiktoken",
-	config = function()
-		require("CopilotChat").setup({
-			system_prompt =
-			"You are a Python expert. Always adhere strictly to the coding style, conventions, and patterns found in the current file and workspace. Keep answers concise.",
-		})
-
-		vim.keymap.set("n", "<leader>cc", "<cmd>CopilotChatToggle<cr>", { desc = "Toggle Copilot Chat" })
-		vim.keymap.set("v", "<leader>ce", "<cmd>CopilotChatExplain<cr>", { desc = "Copilot Explain Selection" })
-	end,
 }
